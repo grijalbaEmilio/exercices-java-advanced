e80c3716d62762af7707ae4f4570678200043eed
Luis Emilio Grijalba <luisegrijalba8@gmail.com>
2022-10-16 19:03:52 -0500

    exceptions exercices

---
+++ exeptions-exercices/src/Main.java
@@ -0,0 +1,44 @@
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println();
+        try {
+            division(3, 0);
+
+        } catch (ArithmeticException e) {
+            System.out.println("arithmetic exeption: " + e.getMessage());
+        }
+
+        try {
+            bucleInArray(1, 2, 3, 4, 5, 6);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("index exception: " + e.getMessage());
+        }
+
+        try {
+            InputStream myFile = new FileInputStream("notFound/path");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("file exception: " + e.getMessage());
+        }
+        System.out.println();
+    }
+
+    static double division(double numerator, double denominator) throws ArithmeticException {
+        if (denominator == 0) {
+            throw new ArithmeticException("imposible dividir por cero");
+        }
+        return numerator / denominator;
+    }
+
+    static void bucleInArray(int... numbers) throws ArrayIndexOutOfBoundsException {
+        for (int i = 0; i < 9; i++) {
+            int nn = numbers[i];
+            continue;
+        }
+    }
+}
